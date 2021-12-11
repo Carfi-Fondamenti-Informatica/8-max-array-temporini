3409b3439759709057ae8ea4f5a8754232002b54
temporini <91788264+temporini@users.noreply.github.com>
2021-12-11 19:49:09 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+float maxarray(float array[],int dim){
 
+    array[dim];
+    float max= array[0];
+    for(int i=0; i<dim;i++){
+        if(max<numeri[i]){
+            max= numeri[i];
+        }
+    }
+    return max;
+}
