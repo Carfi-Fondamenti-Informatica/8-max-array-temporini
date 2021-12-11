545c5fbb7c85e1e90beb70c01bb45542a3e48763
temporini <91788264+temporini@users.noreply.github.com>
2021-12-11 20:34:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 #include "lib.h"
 using namespace std;
 
-
 int main() {
-    int n;
-    cin>> n;
-    float array[n];
-    for(int i=0; i <n; i++){
-        cin>> array[i];
 
+    int n;
+    cin >> n;
+    float array[n];
+
+    for(int i=0; i<n ; i++){
+        cin >> array[i];
     }
-      nmax(array,n);
-    cout << nmax(array,n) <<endl;
+
+    cout << maggiore(n,array) << endl;
+
     return 0;
 }
