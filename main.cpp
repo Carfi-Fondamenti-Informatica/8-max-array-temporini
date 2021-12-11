df59d40dd58dedcff8fe9c37dd4611ac1514c305
temporini <91788264+temporini@users.noreply.github.com>
2021-12-11 19:51:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+    int num;
+    cin>> num;
+    float array[num];
+    for(int i=0; i <num; i++){
+        cin>> array[i];
+
+    }
+
+    cout << maxarray(array,num) <<endl;
+    return 0;
 }
