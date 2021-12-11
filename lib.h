b5d2510bc08cdedffe09918405594fd8d008afe9
temporini <91788264+temporini@users.noreply.github.com>
2021-12-11 20:42:28 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,4 @@
-
-#ifndef MATRICI_LIB_H
-#define MATRICI_LIB_H
-  float maggiore(int dim, float lista[])
-#endif //MATRICI_LIB_H
-
+#ifndef COMPITO8MAXARRAY_LIB_H
+#define COMPITO8MAXARRAY_LIB_H
+float maggiore(int dim, float lista[]);
+#endif //COMPITO8MAXARRAY_LIB_H
